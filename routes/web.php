573f1f89fbcf95ba1f0358d3984ef886ccc84bd3
Mosharaf456 <mosharaf111hossain@gmail.com>
2022-01-15 00:46:03 +0600

    cls11 route comments how to work

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*Route works top to bottom and left to right */
 Route::get('/', function () {
     return view('welcome');
 });
